9d03fe0a52432c697e8ff8aa1ecf7716e7735d5b
Sophian <sophian@achiba.com>
2017-08-07 12:47:25 -0700

    Initial Commit

---
+++ Parse Instagram Clone/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Parse Instagram Clone
+//
+//  Created by Sophian on 8/7/17.
+//  Copyright © 2017 Sophian. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
